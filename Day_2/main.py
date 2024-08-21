35678ce15bada214d82c6cecb1b6405827e28304
Eric Collier <ericscollier@gmail.com>
2024-08-21 16:49:13 -0400

    Day 2 Part 1 works

---
+++ Day_2/main.py
@@ -0,0 +1,44 @@
+import os
+
+def read_file(filepath:str) -> list:
+    with open(filepath, "r") as f:
+        data = f.read().splitlines()
+    return data
+
+def solve_p1(filepath:str) -> str:
+    data = read_file(filepath)
+
+    hor_pos = 0
+    depth = 0
+    for row in data:
+        direction, value = row.split(' ')
+        value = int(value)
+        if direction == 'forward':
+            hor_pos += value
+        if direction == 'down':
+            depth += value
+        if direction == 'up':
+            depth += -1 * value
+
+    return hor_pos * depth
+
+
+def solve_p2(filepath:str) -> str:
+    data = read_file(filepath)
+    
+    ...
+
+def main():
+    test_filepath = os.path.join(os.path.dirname(__file__) , 'test_input.txt')
+    input_filepath = os.path.join(os.path.dirname(__file__) , 'input.txt')
+
+    print(f'Test p1: {solve_p1(test_filepath)}')
+    print(f'Real p1: {solve_p1(input_filepath)}')
+
+    # print(f'Test p2: {solve_p2(test_filepath)}')
+    # print(f'Real p2: {solve_p2(input_filepath)}')    
+
+    ...
+
+if __name__ == "__main__":
+    main()
