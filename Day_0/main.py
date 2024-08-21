a615a99d49873c8d500907c00f142e00cdb12aeb
Eric Collier <ericscollier@gmail.com>
2024-08-21 15:40:23 -0400

    update template

--- Day_0/main.py
+++ Day_0/main.py
@@ -5,10 +5,26 @@ def read_file(filepath:str) -> list:
         data = f.read().splitlines()
     return data
 
+def solve_p1(filepath:str) -> str:
+    data = read_file(filepath)
+    data = [int(x) for x in data]
+
+    ...
+
+def solve_p2(filepath:str) -> str:
+    data = read_file(filepath)
+    data = [int(x) for x in data]
+    ...
 
 def main():
     test_filepath = os.path.join(os.path.dirname(__file__) , 'test_input.txt')
     input_filepath = os.path.join(os.path.dirname(__file__) , 'input.txt')
+
+    print(f'Test p1: {solve_p1(test_filepath)}')
+    print(f'Real p1: {solve_p1(input_filepath)}')
+
+    # print(f'Test p2: {solve_p2(test_filepath)}')
+    # print(f'Real p2: {solve_p2(input_filepath)}')    
 
     ...
 
